30c3fb23c1562e407e39a92e8c5abfbad0c63a6f
Oleg <Olegius055@gmail.com>
2025-10-20 23:44:19 +0200

    change style, update ability template

--- quartz/plugins/transformers/dsElements.ts
+++ quartz/plugins/transformers/dsElements.ts
@@ -29,39 +29,33 @@ export const DsElements: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                         ${data.flavor ? `<div class="ds-ability-card-description"><em>${data.flavor}</em></div>` : ""}
                         <hr class="card-separator" />
                         <div class="ds-ability-card-upper-properties">
-                            <div>${data.keywords ?? "-"}</div>
-                            <div>${data.type ?? "-"}</div>
+                          ${data.keywords ? `<div>${data.keywords}</div>` : ""}
+                          ${data.type ? `<div>${data.type}</div>` : ""}
                         </div>
                         <div class="ds-ability-card-lower-properties">
-                            <div>📏 ${data.distance ?? "-"}</div>
-                            <div>🎯 ${data.target ?? "-"}</div>
+                          ${data.distance ? `<div>📏${data.distance}</div>` : ""}
+                          ${data.target ? `<div>🎯${data.type}</div>` : ""}
                         </div>
-                        ${
-                          data.effects
-                            ? data.effects
-                                .map((element: any) => {
-                                  if (element.roll) {
-                                    const rollContainer = `
-                                        <div class="ds-ability-card-rolls ds-ability-card-effect">
-                                        <div class="ds-ability-card-roll-label">${element.roll}</div>
-                                        <div class="ds-ability-card-roll-line"><div class="tier-key-container t1"><div class="key-body t1"><div class="key-body-text">≤11</div></div></div><span><p>${element.t1 ?? element["11 or lower"] ?? "-"}</p></span></div>
-                                        <div class="ds-ability-card-roll-line"><div class="tier-key-container t2"><div class="key-body t2"><div class="key-body-text">12-16</div></div></div><span><p>${element.t2 ?? element["12-16"] ?? "-"}</p></span></div>
-                                        <div class="ds-ability-card-roll-line"><div class="tier-key-container t3"><div class="key-body t3"><div class="key-body-text">17+</div></div></div><span><p>${element.t3 ?? element["17+"] ?? "-"}</p></span></div>
-                                    </div>`
-                                    return rollContainer
-                                  } else if (element instanceof Object) {
-                                    return Object.keys(element)
-                                      .map((key) => {
-                                        return `<div class="ds-ability-card-effect"><strong>${key}:</strong> ${element[key]}</div>`
-                                      })
-                                      .join("")
-                                  } else {
-                                    return `<div>${element}</div>`
-                                  }
-                                })
-                                .join("")
-                            : ""
-                        }
+                        ${data.effects ? data.effects.map((element: any) => {
+                    if (element.roll) {
+                      const rollContainer =
+                        `<div class="ds-ability-card-rolls ds-ability-card-effect">
+                          <div class="ds-ability-card-roll-label">${element.roll}</div>
+                          <div class="ds-ability-card-roll-line"><div class="tier-key-container t1"><div class="key-body t1"><div class="key-body-text">≤11</div></div></div><span><p>${element.t1 ?? element["11 or lower"] ?? "-"}</p></span></div>
+                          <div class="ds-ability-card-roll-line"><div class="tier-key-container t2"><div class="key-body t2"><div class="key-body-text">12-16</div></div></div><span><p>${element.t2 ?? element["12-16"] ?? "-"}</p></span></div>
+                          <div class="ds-ability-card-roll-line"><div class="tier-key-container t3"><div class="key-body t3"><div class="key-body-text">17+</div></div></div><span><p>${element.t3 ?? element["17+"] ?? "-"}</p></span></div></div>`
+                      return rollContainer
+                    } else if (element instanceof Object) {
+                      return Object.keys(element)
+                        .map((key) => {
+                          return `<div class="ds-ability-card-effect"><strong>${key}:</strong> ${element[key]}</div>`
+                        })
+                        .join("")
+                    } else { return `<div>${element}</div>` }
+                  })
+                      .join("")
+                      : " "
+                    }
                       </div>
                     `
                   parent.children[index] = {
